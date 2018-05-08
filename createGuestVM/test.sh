9b40a517f5425a80ca93883ae3e777862bd3d324
youngju lee <leeyj7141@gmail.com>
2018-05-08 10:21:53 +0900

    쓸모없는것들 정리함.

--- createGuestVM/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-
-virt-install --import --name ci-test1 --ram 4096 --vcpus 2 --os-type linux --os-variant rhel7 --cpu host-passthrough --disk /youngju/vms//ci-test1.img,format=qcow2,bus=virtio --disk /youngju/vms//ci-test1-cloud-init.iso,device=cdrom --network bridge=youngju-net-10,model=virtio --graphics none --hvm --virt-type kvm --noautoconsole
